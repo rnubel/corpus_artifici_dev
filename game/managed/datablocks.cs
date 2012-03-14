2c183cb67d047402d962d7ff18aae9f063da7e42
rnubel <rnubel2@illinois.edu>
2012-03-13 20:10:34 -0500

    Initial commit, plus a few sprite resources.

---
+++ game/managed/datablocks.cs
@@ -0,0 +1,75 @@
+$managedDatablockSet = new SimSet() {
+   canSaveDynamicFields = "1";
+      setType = "Datablocks";
+
+   new t2dImageMapDatablock(platformer_sprites_pixelizedImageMap) {
+      imageName = "~/data/images/platformer_sprites_pixelized.png";
+      imageMode = "CELL";
+      frameCount = "-1";
+      filterMode = "SMOOTH";
+      filterPad = "1";
+      preferPerf = "1";
+      cellRowOrder = "1";
+      cellOffsetX = "0";
+      cellOffsetY = "0";
+      cellStrideX = "0";
+      cellStrideY = "0";
+      cellCountX = "8";
+      cellCountY = "9";
+      cellWidth = "64";
+      cellHeight = "64";
+      preload = "1";
+      allowUnload = "0";
+      force16Bit = "0";
+   };
+   new t2dAnimationDatablock(basicIdle) {
+      imageMap = "platformer_sprites_pixelizedImageMap";
+      animationFrames = "0 1 2 3";
+      animationTime = "0.666667";
+      animationCycle = "1";
+      randomStart = "0";
+      startFrame = "0";
+      animationPingPong = "0";
+      animationReverse = "0";
+   };
+   new t2dAnimationDatablock(basicRun) {
+      imageMap = "platformer_sprites_pixelizedImageMap";
+      animationFrames = "4 5 6 7 8 9 10 11";
+      animationTime = "0.571429";
+      animationCycle = "1";
+      randomStart = "0";
+      startFrame = "0";
+      animationPingPong = "0";
+      animationReverse = "0";
+   };
+   new t2dAnimationDatablock(basicWalk) {
+      imageMap = "platformer_sprites_pixelizedImageMap";
+      animationFrames = "32 33 34 35 36 37 38 39";
+      animationTime = "0.666667";
+      animationCycle = "1";
+      randomStart = "0";
+      startFrame = "0";
+      animationPingPong = "0";
+      animationReverse = "0";
+   };
+   new t2dImageMapDatablock(tilemap_1ImageMap) {
+      imageName = "~/data/images/tilemap_1.png";
+      imageMode = "CELL";
+      frameCount = "-1";
+      filterMode = "SMOOTH";
+      filterPad = "1";
+      preferPerf = "1";
+      cellRowOrder = "1";
+      cellOffsetX = "0";
+      cellOffsetY = "0";
+      cellStrideX = "65";
+      cellStrideY = "65";
+      cellCountX = "-1";
+      cellCountY = "-1";
+      cellWidth = "64";
+      cellHeight = "64";
+      preload = "1";
+      allowUnload = "0";
+      force16Bit = "0";
+   };
+};
