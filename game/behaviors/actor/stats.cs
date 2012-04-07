49f98783d967f387613b4ffaa0c9c8ad93bb06ed
Nyarlathotep <darkhitman@gmail.com>
2012-04-07 12:57:28 -0500

    Adding basic stats class
    Signed-off-by: Nyarlathotep <darkhitman@gmail.com>

---
+++ game/behaviors/actor/stats.cs
@@ -0,0 +1,52 @@
+function StatsBehavior:onLevelLoaded(%this)
+{
+/* Attributes */
+	%this.Strength = 0;
+	%this.Dexterity = 0;
+	%this.Constitution = 0;
+	%this.Wisdom = 0;
+	%this.Intelligence = 0;
+	%this.Charisma = 0;
+	
+/* Effective Stats */
+%this.Health = 0;
+%this.Stamina = 0;
+%this.AttackSpeed = 0;
+%this.ManufactureSpeed = 0;
+%this.PrepareSpeed = 0;
+
+%this.BlockPhysical = 0;
+%this.BlockFire = 0;
+%this.BlockIce = 0;
+%this.BlockNano = 0;
+
+%this.ResistPhysical = 0;
+%this.ResistFire = 0;
+%this.ResistIce = 0;
+%this.ResistNano = 0;
+
+%this.DamagePhysical = 0;
+%this.DamageFire = 0;
+%this.DamageIce = 0;
+%this.DamageNano = 0;
+
+%this.MaxKnockbackTime = 0;
+%this.HitForce = 0;
+%this.BlockForce = 0;
+%this.ForwardMoveSpeed = 0;
+%this.BackwardMoveSpeed = 0;
+%this.RollForce = 0;
+%this.TurnaroundTime = 0;
+%this.FallRecoveryTime = 0;
+%this.AttackFailRecoveryTime = 0;
+%this.ParryTime = 0;
+%this.ParryRecoveryTime = 0;
+%this.JumpXForce = 0;
+%this.JumpYForce = 0;
+%this.StaminaRegenRate = 0;
+%this.HealthRegenRate = 0;
+%this.StaminaRegenWhileBlocking = 0;
+%this.StaminaCostCoefficient = 0;
+
+
+}
